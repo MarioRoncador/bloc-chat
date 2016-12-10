4363e9fe6bc5eb4970471c99c142c882f5ca498f
Ryan Balfanz <ryan@ryanbalfanz.net>
2016-12-10 15:02:11 -0800

    Wire basic form

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -3,10 +3,10 @@
          this.homeTitle = "Bloc Chat";
          this.rooms = Room.all;
          //Room.addRoom("Chat Room 3");
-         
+
          this.open = function (size, parentSelector) {
              console.log("Attempting to open modal window.");
-            
+
              var modalInstance = $uibModal.open({
                 ariaLabelledBy: 'modal-title',
                 ariaDescribedBy: 'modal-body',
@@ -21,21 +21,20 @@
 //                  }
                 }
             });
-             
-            modalInstance.result.then(function (newRoomName) {
+
+            modalInstance.result.then(function (newRoomInfo) {
 //                $ctrl.selected = selectedItem;
-                console.log("The new room name is: " + newRoomName);
+                console.log("The new room name is: " + newRoomInfo.name);
                 // Make the new room in Firebase here, based on NewRoomName from the modal
-                
             }, function () {
                 console.log('modal-component dismissed at: ' + new Date());
             });
-             
-             
-         
+
+
+
          };
      };
-     
+
      angular
          .module('blocChat')
          .controller('HomeCtrl', ['$uibModal', 'Room', HomeCtrl]);
--- app/scripts/controllers/ModalInstanceCtrl.js
+++ app/scripts/controllers/ModalInstanceCtrl.js
@@ -1,37 +1,48 @@
  (function() {
      function ModalInstanceCtrl($uibModalInstance) {
-         this.ModalTitle = "New Chat Room Name"
-         //this.rooms = Room.all;
-         //Room.addRoom("Chat Room 3");    
-      /*   var $ctrl = this;
-             var modalInstance = $uibModal.open({
-             animation: $ctrl.animationsEnabled,
-             ariaLabelledBy: 'modal-title',
-             ariaDescribedBy: 'modal-body',
-             templateUrl: 'templates/modal.html',
-             size: size,
-             appendTo: parentElem,
-             resolve: {
-                 items: function () {
-                    return $ctrl.;
-                 }   
-             }
-        });
-        
-        
-*/
-         
-           this.ok = function () {
-                $uibModalInstance.close('Ryan');
-           };
+         var DEBUGABLE = false;
+         var DEBUG_ENABLED = true;
+         var TITLE = 'Create New Room';
+         var vm = this;
 
-           this.cancel = function () {
-                $uibModalInstance.dismiss('cancel');
-           };
-  
-            console.log("From IN the ModalInstanceCtrl");
+         vm.cancel = cancel;
+         vm.debug = DEBUGABLE && DEBUG_ENABLED;
+         vm.debuggable = DEBUGABLE;
+         vm.newRoom = {};
+         vm.newRoomMaster = {};
+         vm.ok = submitForm;
+         vm.reset = resetForm;
+         vm.submit = submitForm;
+         vm.toggleDebug = toggleDebug;
+         this.ModalTitle = TITLE;
+
+          activate();
+
+           function activate() {
+             console.log("From IN the ModalInstanceCtrl");
+           }
+
+           function cancel() {
+             vm.reset();
+             $uibModalInstance.dismiss('cancel');
+           }
+
+           function resetForm() {
+             console.log("Resetting form");
+             vm.newRoom = angular.copy(vm.newRoomMaster);
+           }
+
+           function submitForm() {
+             console.log("Submitting form");
+             $uibModalInstance.close(vm.newRoom);
+           }
+
+           function toggleDebug() {
+             console.log("Toggleing debug mode");
+             vm.debug = !vm.debug;
+           }
      }
- 
+
      angular
          .module('blocChat')
          .controller('ModalInstanceCtrl', ['$uibModalInstance', ModalInstanceCtrl]);
