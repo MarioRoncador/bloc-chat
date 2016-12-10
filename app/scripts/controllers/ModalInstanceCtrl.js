f4b5883beb0f8f3951c943b56317db1ec7842e16
Ryan Balfanz <ryan@ryanbalfanz.net>
2016-12-10 15:03:30 -0800

    beautify

--- app/scripts/controllers/ModalInstanceCtrl.js
+++ app/scripts/controllers/ModalInstanceCtrl.js
@@ -1,49 +1,49 @@
- (function() {
-     function ModalInstanceCtrl($uibModalInstance) {
-         var DEBUGABLE = false;
-         var DEBUG_ENABLED = true;
-         var TITLE = 'Create New Room';
-         var vm = this;
+(function() {
+    function ModalInstanceCtrl($uibModalInstance) {
+        var DEBUGABLE = false;
+        var DEBUG_ENABLED = true;
+        var TITLE = 'Create New Room';
+        var vm = this;
 
-         vm.cancel = cancel;
-         vm.debug = DEBUGABLE && DEBUG_ENABLED;
-         vm.debuggable = DEBUGABLE;
-         vm.newRoom = {};
-         vm.newRoomMaster = {};
-         vm.ok = submitForm;
-         vm.reset = resetForm;
-         vm.submit = submitForm;
-         vm.toggleDebug = toggleDebug;
-         this.ModalTitle = TITLE;
+        vm.cancel = cancel;
+        vm.debug = DEBUGABLE && DEBUG_ENABLED;
+        vm.debuggable = DEBUGABLE;
+        vm.newRoom = {};
+        vm.newRoomMaster = {};
+        vm.ok = submitForm;
+        vm.reset = resetForm;
+        vm.submit = submitForm;
+        vm.toggleDebug = toggleDebug;
+        this.ModalTitle = TITLE;
 
-          activate();
+        activate();
 
-           function activate() {
-             console.log("From IN the ModalInstanceCtrl");
-           }
+        function activate() {
+            console.log("From IN the ModalInstanceCtrl");
+        }
 
-           function cancel() {
-             vm.reset();
-             $uibModalInstance.dismiss('cancel');
-           }
+        function cancel() {
+            vm.reset();
+            $uibModalInstance.dismiss('cancel');
+        }
 
-           function resetForm() {
-             console.log("Resetting form");
-             vm.newRoom = angular.copy(vm.newRoomMaster);
-           }
+        function resetForm() {
+            console.log("Resetting form");
+            vm.newRoom = angular.copy(vm.newRoomMaster);
+        }
 
-           function submitForm() {
-             console.log("Submitting form");
-             $uibModalInstance.close(vm.newRoom);
-           }
+        function submitForm() {
+            console.log("Submitting form");
+            $uibModalInstance.close(vm.newRoom);
+        }
 
-           function toggleDebug() {
-             console.log("Toggleing debug mode");
-             vm.debug = !vm.debug;
-           }
-     }
+        function toggleDebug() {
+            console.log("Toggleing debug mode");
+            vm.debug = !vm.debug;
+        }
+    }
 
-     angular
-         .module('blocChat')
-         .controller('ModalInstanceCtrl', ['$uibModalInstance', ModalInstanceCtrl]);
- })();
+    angular
+        .module('blocChat')
+        .controller('ModalInstanceCtrl', ['$uibModalInstance', ModalInstanceCtrl]);
+})();
