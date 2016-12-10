44b2de1ea908175f5ed7b67a71ce02d7ef88b8a3
Ryan Balfanz <ryan@ryanbalfanz.net>
2016-12-10 15:04:01 -0800

    Remove dead code

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -2,7 +2,6 @@
      function HomeCtrl($uibModal, Room) {
          this.homeTitle = "Bloc Chat";
          this.rooms = Room.all;
-         //Room.addRoom("Chat Room 3");
 
          this.open = function (size, parentSelector) {
              console.log("Attempting to open modal window.");
@@ -13,25 +12,16 @@
                 templateUrl: '/templates/modal.html',
                 controller: 'ModalInstanceCtrl',
                 controllerAs: '$ctrl',
-//              size: size,
-//              appendTo: parentElem,
             resolve: {
-//               items: function () {
-//               return $ctrl.items;
-//                  }
                 }
             });
 
             modalInstance.result.then(function (newRoomInfo) {
-//                $ctrl.selected = selectedItem;
                 console.log("The new room name is: " + newRoomInfo.name);
                 // Make the new room in Firebase here, based on NewRoomName from the modal
             }, function () {
                 console.log('modal-component dismissed at: ' + new Date());
             });
-
-
-
          };
      };
 
