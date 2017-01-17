45ae3f15d7fc23123fe9a718eb4cdc5233b529ce
Ryan Balfanz <ryan@ryanbalfanz.net>
2017-01-16 22:57:02 -0800

    Add additional logging to `Messages.addMessage`

--- app/scripts/services/Message.js
+++ app/scripts/services/Message.js
@@ -2,26 +2,31 @@
   function Message($firebaseArray) {
 
     var room = "-KY4gRoWdWnpb7goJZgS";
-      
-    var ref = firebase.database().ref().child("messages").orderByChild("roomId").equalTo(room); 
+
+    var ref = firebase.database().ref().child("messages").orderByChild("roomId").equalTo(room);
     var messages = $firebaseArray(ref);
-    
+
     var date = new Date().getTime();
     d = new Date(date);
-     
-    
+
+
     return {
       all: messages,
       addMessage: function(sender, content, roomId) {
-        messages.$add({
+        var msg = "Adding message to room " + roomId + " as " + sender + ":\n\t" + content;
+        console.info(msg);
+        var messageData = {
             createdAt: d.toString(),
             sender: sender,
             content: content,
             roomId: roomId,
-        });
+        };
+        console.info("Message data:");
+        console.info(messageData);
+        messages.$add(messageData);
       },
     };
-    
+
   }
 
   angular
